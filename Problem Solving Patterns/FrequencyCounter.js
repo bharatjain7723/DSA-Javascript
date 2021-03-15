32ef87dafc4469c96d48a38104241aef9eac1e1a
Bharat Jain <bharatjain7723@gmail.com>
2021-03-15 18:21:04 +0530

    Frequency counter problems

--- Problem Solving Patterns/FrequencyCounter.js
+++ Problem Solving Patterns/FrequencyCounter.js
@@ -60,7 +60,7 @@ function validAnagram(str1, str2) {
 
     for (let char of str2) {
         // freq2[char] = ++freq2[char] || 1;
-        if(!lookup[char]) {
+        if (!lookup[char]) {
             return false;
         } else {
             lookup[char]--;
@@ -69,3 +69,42 @@ function validAnagram(str1, str2) {
 
     return true;
 }
+
+//********************************************************************************************* */
+
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+//
+// Examples:
+
+areThereDuplicates(1, 2, 3); // false
+areThereDuplicates(1, 2, 2); // true
+areThereDuplicates("a", "b", "c", "a"); // true
+
+// Restrictions:
+
+// Time - O(n)
+// Space - O(n)
+
+
+function areThereDuplicates(...arr) {
+    // good luck. (supply any arguments you deem necessary.)
+
+    // Frequency counter
+    if (arr.length === 0) return null;
+
+    let freq = {};
+
+    for (let el of arr) {
+        if (freq[el]) {
+            return true;
+        } else {
+            freq[el] = 1;
+        }
+    }
+
+    return false;
+}
+
+// NOTE: There is a way to do this by Multiple Pointer in Time: O(nlogn) and Space of O(1)
+
